f76036d83e6434b7e34dff5f3e6e0354ddb274df
ranasarkar90 <mdranasarkar909@gmail.com>
2023-04-01 22:42:34 +0600

    java-class-1

---
+++ hello.js
@@ -0,0 +1,33 @@
+
+
+var strType ='Hello world';
+console.log( strType)
+
+var numType = "303030"
+console.log(typeof numType)
+
+var myFd ="Ripon Jahidul Jabed Esa"
+console.log(myFd)
+
+var num1 =55;
+var num2 =99;
+var mot = num1 + num2
+console.log(mot)
+
+let x = 10;
+let y = 20;
+let sum = x*y;
+console.log (sum)
+
+let a =12;
+let b =23;
+let c =30;
+let d =35;
+let num3 = a+b+c+d;
+console.log(num3)
+
+var rana =4;
+var ripon =5;
+var jahid =5;
+var RJR = rana*ripon*jahid;
+console.log(RJR)
